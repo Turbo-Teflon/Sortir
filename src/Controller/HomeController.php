dd18ff0f25873a8110884cec3b47a7110c75ef45
aurel12321 <aurelien_coeuru@msn.com>
2025-09-03 13:40:11 +0200

    vue home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
 
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function home(SortieRepository $sortieRepository): Response
     {
-        return $this->redirectToRoute('app_login');
+
+        $sorties = $sortieRepository->findBy(
+            ['etat' => 'Ouverte'],
+            ['startDateTime' => 'DESC'],
+            5
+        );
+
+        return $this->render('sortie/home.html.twig', [
+            'sorties' => $sorties,
+        ]);
     }
 
 
